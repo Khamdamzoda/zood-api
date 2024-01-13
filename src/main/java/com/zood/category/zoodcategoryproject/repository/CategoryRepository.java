10ac86a12681820c9aa38a3dff58b56c37e229e3
Khamdamzoda <67256521+Khamdamzoda@users.noreply.github.com>
2024-01-13 19:14:02 +0500

    Delete src/main directory

--- src/main/java/com/zood/category/zoodcategoryproject/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zood.category.zoodcategoryproject.repository;
-
-import com.zood.category.zoodcategoryproject.model.ItemCategory;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CategoryRepository extends MongoRepository<ItemCategory, String> {
-}
